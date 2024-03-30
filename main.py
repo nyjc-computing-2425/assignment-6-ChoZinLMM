9bbf09104f972614ff1ee1dd0f0f06d35c664630
ChoZinLMM <160577451+ChoZinLMM@users.noreply.github.com>
2024-03-30 06:35:42 +0000

    Final submission

--- main.py
+++ main.py
@@ -1,32 +1,57 @@
-from datetime import datetime
 import time
 
 # Part 1
 def clock(n):
     """
-    """
+    Parameters
+    ----------
+    n = int
+      represents the number of seconds
+
+    Prints
+    ------
+    The time every second, for n number of seconds
+      """
     seconds = 0
     while seconds < n:
-      print(time.strftime("%H:%M:%S"))
+      print(time.strftime("%H:%M:%S"), end = "\r")
       seconds += 1
       time.sleep(1)
 
 # Part 2
 def lcm(a, b):
     """
+    Function to find the lowest common multiple of a and b
+    Parameters
+    ----------
+    a = integer
+    b = integer
+
+    Returns
+    --------
+    The lowest common multiple of a and b
     """
+    a_original = a
+    b_original = b
     while a != b:
       if a < b:
-        a += a
+        a += a_original
       elif b < a:
-        b += b
-    else:
-      return(a)
+        b += b_original
+    
+    return(a)
 
-
-# Part 3
+#Part 3
 def gcf(a, b):
     """
+    Parameters
+    ----------
+    a = int
+    b = int
+
+    Returns
+    -------
+    The greatest common factor of a and b
     """
     if b == 0:
       return(a)
@@ -34,10 +59,6 @@ def gcf(a, b):
       while b != 0:
         r = a % b
         a = b
-        b = r
-        if b == 0:
-          break
-      else:
-        return(a)
-
-  
+        b = r   
+      
+      return(a)

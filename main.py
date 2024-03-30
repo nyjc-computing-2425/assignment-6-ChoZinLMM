fe8efe56f7251be67d41954f1721c2930d461f04
ChoZinLMM <160577451+ChoZinLMM@users.noreply.github.com>
2024-03-30 04:36:32 +0000

    Draft commit

--- main.py
+++ main.py
@@ -1,20 +1,43 @@
 from datetime import datetime
 import time
 
-
 # Part 1
 def clock(n):
-    # Your code here
-    pass
-
+    """
+    """
+    seconds = 0
+    while seconds < n:
+      print(time.strftime("%H:%M:%S"))
+      seconds += 1
+      time.sleep(1)
 
 # Part 2
 def lcm(a, b):
-    # Your code here
-    pass
+    """
+    """
+    while a != b:
+      if a < b:
+        a += a
+      elif b < a:
+        b += b
+    else:
+      return(a)
 
 
 # Part 3
 def gcf(a, b):
-    # Your code here
-    pass
+    """
+    """
+    if b == 0:
+      return(a)
+    else:
+      while b != 0:
+        r = a % b
+        a = b
+        b = r
+        if b == 0:
+          break
+      else:
+        return(a)
+
+  
